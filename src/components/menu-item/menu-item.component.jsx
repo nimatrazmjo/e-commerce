a2d5236f686dbe45180eff4e32c49bc323b597bd
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2021-12-26 22:05:12 +0430

    Fix hompage style,broken into small component

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import './menu-item.style.scss';
+
+const MenuItem = ({title, size, imageUrl}) => (
+    <div 
+    
+    className={`${size} menu-item`} >
+        <div className="background-image"
+        style={{
+            backgroundImage:`url(${imageUrl})`
+        }}
+        >
+        <div className="content">
+            <h1 className="title"> {title.toUpperCase()} </h1>
+            <span className="subtitle">Shop Now</span>
+        </div>
+    </div>
+</div>
+)
+
+export default MenuItem;
