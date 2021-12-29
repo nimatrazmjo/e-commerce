cfacd620440d2c165343e60f6953ffac3d39d511
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2021-12-29 23:06:13 +0430

    Add header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import HomePage from './pages/homepage/homepage.component';
 
 import { Routes, Route } from 'react-router-dom'
 import Shop from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/shop' element={<Shop />} />
