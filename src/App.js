97dc095d2a3ae3ee6790f41991333ab943d3afa3
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2021-12-30 22:47:20 +0430

    Create signin-and-signup page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from './pages/homepage/homepage.component';
 import { Routes, Route } from 'react-router-dom'
 import Shop from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/signin-and-signup/signin-and-signup.component';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/shop' element={<Shop />} />
+        <Route path='/login' element={<SignInAndSignUp />} />
       </Routes>
     </div>
   );
---
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import './signin-and-signup.style.scss';
+
+const SignInAndSignUp = () => (
+    <div>Sign In / Sign Up</div>
+)
+
+export default SignInAndSignUp;
