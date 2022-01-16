0715faf0f7c02de30dd50025171be4225ebf1f5c
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2022-01-16 23:33:41 +0430

    Add cartIcon and cartDropdown components to our application, and also implement cart reducer

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import CustomButton from '../custom-button/custom-button.component';
+
+import './cart-dropdown.style.scss';
+
+const CartDropDown = () => (
+<div className="cart-dropdown">
+    <div className="cart-items"/>
+
+    <CustomButton />
+</div>
+)
+
+export default CartDropDown
