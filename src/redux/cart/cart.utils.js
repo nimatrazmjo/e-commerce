1485371bda5e665d19dd6ab86560947e25ec94c9
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2022-01-25 21:50:22 +0430

    Add item and remote item functionality

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -5,3 +5,20 @@ export const addItemToCart = (cartItems, cartItemsToAdd) => {
     }
     return [...cartItems, {...cartItemsToAdd, quantity: 1}];
 }
+
+export const RemoveItemFromCart = (cartItems, cartItemsToRemove) => {
+    const existringCartItem = cartItems.find(item => item.id === cartItemsToRemove.id);
+
+    if (existringCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemsToRemove.id);
+    }
+    if (existringCartItem.quantity >= 1 ) {
+        return cartItems.map(item => item.id === cartItemsToRemove.id ? { ...item, quantity: item.quantity-1}: item);
+    }
+    return [...cartItems, {...cartItemsToRemove, quantity: 1}];
+}
+
+
+export const clearItemFromCart = (cartItems, item) => {
+    return cartItems.filter(cartItem => cartItem.id !== item.id)
+}
