06df63ab8daeea68700605a7530108ed0bd0e5c6
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2022-01-31 22:36:38 +0430

    add margin x to shop item list

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -9,7 +9,7 @@ const CollectionPreview = ({title, items}) => {
             <h1 className="title">{title.toUpperCase()}</h1>
             <div className="preview">
                 {
-                    items.filter(({id})=>id<4).map((item)=><CollectionItem key={item.id}  item={item}/>)
+                    items.filter((item,idx)=>idx<4).map((item)=><CollectionItem key={item.id}  item={item}/>)
                 }
             </div>
         </div>
